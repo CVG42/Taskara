50be868c00a0eb28a18aa2a503429fe11aad8846
CVG42 <zap653@amerike.edu.mx>
2025-07-06 23:13:59 -0600

    TaskUIController script creation

---
+++ Taskara/Assets/_Project/Scripts/UI/TaskUIController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Taskara.UI
+{
+    public class TaskUIController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
